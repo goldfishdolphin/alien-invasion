f5e07fbd7a707908c6ca8880e342f749159ce08c
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-04 18:47:57 +0000

    Create a setting classfor the game display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,16 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assests and behavior."""
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (0, 50, 98)
+        
         
 
     def run_game(self):
@@ -18,7 +20,7 @@ class AlienInvasion:
                     sys.exit()
 
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
