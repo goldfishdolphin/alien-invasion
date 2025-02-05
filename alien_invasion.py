def1d480d95dac430554882815b179cce892fa8a
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-05 12:09:53 +0000

    add ship to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assests and behavior."""
@@ -8,8 +9,10 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
         
         
 
@@ -21,6 +24,7 @@ class AlienInvasion:
 
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.bltime()
             pygame.display.flip()
 
 if __name__ == '__main__':
