c7c0a4c8c6048bdaf7f9da6e66303a45a92a7e9f
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-05 12:18:45 +0000

    refactor the code by adding helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,14 +18,18 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
+            self._check_events()
+            self._update_screen()
+
+
+    def _check_events(self):
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     sys.exit()
-
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.bltime()
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bltime()
+        pygame.display.flip()    
 
 if __name__ == '__main__':
     ai= AlienInvasion()
