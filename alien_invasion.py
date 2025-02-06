53ae33a8cef4acefb3c5e1da88f888c19b4a9fd8
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-06 17:33:28 +0000

    move ship right and left

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,12 +13,11 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
-        
-        
 
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
 
@@ -26,6 +25,18 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                    
+                elif event.type == pygame.KEYUP:
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = False
+                        elif event.key == pygame.K_LEFT:
+                            self.ship.moving_left = False
+                        
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.bltime()
