bea81d9d7abc7fd9c59ac8f17262316543e1ea66
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-11 11:06:14 +0000

    change the alien image with transparent background

--- alien.py
+++ alien.py
@@ -1,11 +1,11 @@
 import pygame
-from pygame import sprite
-class Alien(sprite):
+from pygame.sprite import Sprite
+class Alien(Sprite):
     """A class to represent a single alien."""
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-        self.image = pygame.image.load('image/alien.bmp')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
