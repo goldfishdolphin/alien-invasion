33d405d7b0b6384552a887da98108f14f8888ac7
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-13 11:18:14 +0000

    end game and identify when to run the game

--- game_stats.py
+++ game_stats.py
@@ -2,9 +2,10 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_status()
+        self.game_active = True
 
     def reset_status(self):
         self.ships_left = self.settings.ship_limit
 
-        
+
         
