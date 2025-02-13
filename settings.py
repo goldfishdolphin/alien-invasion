eef77440169bb7561759ef0d390d38432b6985a3
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-13 08:02:45 +0000

    make bullet large for testing

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color =(0, 50, 98)
         self.ship_speed = 1.5
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 2.5
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullet_allowed = 3
