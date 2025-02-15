d655cf8f941443657a8e55e066c35752162dec8b
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-15 15:24:24 +0000

    modify the speed setting

--- settings.py
+++ settings.py
@@ -7,11 +7,21 @@ class Settings:
         self.bg_color =(0, 50, 98)
         self.ship_speed = 1.5
         self.ship_limit = 3
-        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
-        self.bullet_allowed = 3
-        self.alien_speed =1.0
+        self.bullet_allowed = 3 
         self.fleet_drop_speed = 10
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self): 
+        self.ship_speed = 1.5
+        self.bullet_speed = 3
+        self.alien_speed =1.0
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
