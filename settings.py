3e380a95aef9a0264340582e5ecd0690e632a2f9
Naureen Imran <naureen.imran123@yahoo.com>
2025-02-16 22:10:35 +0000

    increase points value

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
         self.bullet_allowed = 3 
         self.fleet_drop_speed = 10
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self): 
@@ -26,3 +27,5 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
